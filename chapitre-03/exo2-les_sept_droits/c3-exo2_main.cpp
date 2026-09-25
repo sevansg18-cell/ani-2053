fbd47cd33774b3a1ddfa51696f625cbcdb4fc014
sevansg18-cell <sevansg18@gmail.com>
2026-09-25 13:02:02 +0100

    main.cpp exo2 chap3

---
+++ chapitre-03/exo2-les_sept_droits/c3-exo2_main.cpp
@@ -0,0 +1,32 @@
+#include "NKWindow/NKWindow.h"
+#include "NKWindow/NKMain.h"
+#include "NKLogger/NkSink.h"
+#include "NKTime/NkTime.h"
+#include "NKTime/NkChrono.h"
+
+#include "NKEvent/NkWindowEvent.h"
+#include "NKEvent/NkKeyboardEvent.h"
+
+using namespace nkentseu;
+
+int nkmain(const NkEntryState &state) {
+    NkWindowConfig cfg;
+    cfg.title  = "Ma fenetre";
+    cfg.width  = 1280;
+    cfg.height = 720;
+    cfg.resizable = true;
+    cfg.movable = true;
+    cfg.closable =  true;
+    cfg.minimizable = true;
+    cfg.maximizable = true;
+    cfg.canFullscreen = true;
+    cfg.modal = true;
+
+    NkWindow window(cfg);
+    if (!window.IsOpen()) {
+        logger.Error("[app] creation fenetre echouee");
+        return -1;
+    }
+    while (window.IsOpen()) { /* les evenements arrivent ici */ }
+    return 0;
+}
